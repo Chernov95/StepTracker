91973abac1b9e1269f1a819958b3f7ad33bd5d0e
Filip Cernov <philipchernov95@gmail.com>
2024-05-04 12:24:11 -0700

    Adds async await syntax for fetching steps data

--- StepTracker/HistoryViewModel.swift
+++ StepTracker/HistoryViewModel.swift
@@ -24,45 +24,49 @@ class HistoryViewModel: ObservableObject {
     let bearerToken: String
     init(bearerToken: String) {
         self.bearerToken = bearerToken
-        fetchStepData()
+        Task {
+            await fetchStepData()
+        }
         generateMockWeeklyStepCount()
         generateMockMonthlyStepCount()
     }
     
     
-    func fetchStepData() {
+    func fetchStepData() async {
         guard !bearerToken.isEmpty else {
-               print("Bearer token is empty")
-               return
-           }
-           let stepsURL = URL(string: "https://testapi.mindware.us/steps")!
-           var request = URLRequest(url: stepsURL)
-           request.httpMethod = "GET"
-           request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-           request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
-           print("Bearer token is \(bearerToken)")
-           URLSession.shared.dataTask(with: request) { data, response, error in
-               guard let data = data else {
-                   print("No data returned: \(error?.localizedDescription ?? "Unknown error")")
-                   return
-               }
-               
-               if let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) {
-                   if let decodedResponse = try? JSONDecoder().decode([StepDataResponce].self, from: data) {
-                       DispatchQueue.main.async {
-                           // Handle decoded response as needed
-                           print("decoded responce\(decodedResponse)")
-                       }
-                   } else {
-                       let responseString = String(data: data, encoding: .utf8)
-                       print("Error decoding response: \(responseString ?? "No data")")
-                   }
-               } else {
-                   let responseString = String(data: data, encoding: .utf8)
-                   print("Error response: \(responseString ?? "No data")")
-               }
-           }.resume()
-       }
+            print("Bearer token is empty")
+            return
+        }
+        
+        let stepsURL = URL(string: "https://testapi.mindware.us/steps")!
+        var request = URLRequest(url: stepsURL)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+        print("Bearer token is \(bearerToken)")
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                let responseString = String(data: data, encoding: .utf8)
+                print("Error response: \(responseString ?? "No data")")
+                return
+            }
+            
+            if let decodedResponse = try? JSONDecoder().decode([StepDataResponce].self, from: data) {
+                DispatchQueue.main.async {
+                    // Handle decoded response as needed
+                    print("decoded response: \(decodedResponse)")
+                }
+            } else {
+                let responseString = String(data: data, encoding: .utf8)
+                print("Error decoding response: \(responseString ?? "No data")")
+            }
+        } catch {
+            print("Error fetching step data: \(error.localizedDescription)")
+        }
+    }
    
     //MARK: For testing purposes on simulator
     
